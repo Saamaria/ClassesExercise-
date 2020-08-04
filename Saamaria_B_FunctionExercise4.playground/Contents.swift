ab167be571debc1bda7f7f41ae7f255b58029538
Saamaria <67989662+Saamaria@users.noreply.github.com>
2020-08-04 00:02:43 -0500

    Add files via upload

---
+++ Saamaria_B_FunctionExercise4.playground/Contents.swift
@@ -0,0 +1,17 @@
+var str = "class exercise"
+
+class add2Numbers{
+var a: Int = 3
+var b: Int = 4
+
+func howMuch(a: Int, b: Int) -> Int {
+    return a + b
+    }
+
+}
+
+var addEmUp = add2Numbers()
+
+var total = addEmUp.howMuch(a: 3, b: 4)
+
+var totalNew = addEmUp.a + addEmUp.b
